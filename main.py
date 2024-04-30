554c3db36082bd0f35b8de483e875e4c46d463c3
Anton <abramov1991a@gmail.com>
2024-04-30 23:21:54 +0500

    some updete

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
 import logging
-import os
 
 import requests
-import telegram
+import urllib
+from telegram.ext import Updater
 from time import sleep
 
 from logs import TelegramLogHandler
-from dotenv import load_dotenv
+from environs import Env
 
 
 def main():
-    load_dotenv()
-    bot = telegram.Bot(token=os.getenv('TG_TOKEN'))
+    env = Env()
+    env.read_env()
+    bot = Updater(token=env.str('TG_TOKEN')).bot
     url = f"https://dvmn.org/api/long_polling/"
     headers = {
-        'Authorization': f"Token {os.getenv('DVMN_TOKEN')}",
+        'Authorization': f"Token {env('DVMN_TOKEN')}",
     }
     params = {
         'timestamp': None,
     }
 
     logging.basicConfig(
-        handlers=[TelegramLogHandler(tg_bot=bot, chat_id=os.getenv('CHAT_ID'))]
+        handlers=[TelegramLogHandler(tg_bot=bot, chat_id=env('CHAT_ID'))]
     )
 
     logging.warning("Бот запущен")
@@ -40,13 +41,13 @@ def main():
                 if acceptance:
                     bot.send_message(
                         text=f'У вас проверили работу {lesson_title}\n{lesson_url}\nК сожалению, в работе нашлись ошибки.',
-                        chat_id=os.getenv('CHAT_ID')
+                        chat_id=env('CHAT_ID')
                     )
 
                 else:
                     bot.send_message(
                         text=f'У вас проверили работу {lesson_title}\n{lesson_url}\nПреподователю все понравилось, можно приступать к следующему уроку!',
-                        chat_id=os.getenv('CHAT_ID')
+                        chat_id=env('CHAT_ID')
                     )
 
                 timestamp = last_update['last_attempt_timestamp']
